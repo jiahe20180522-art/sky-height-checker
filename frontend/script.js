97f4c0a8618a0fd2d973b5e3242e74426470e47a
jiahe20180522-art <jiahe20180522@gmail.com>
2025-10-24 17:39:16 +0800

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,15 @@
-const RENDER_URL = "https://sky-height-checker-2.onrender.com/upload";
-
-
 const uploadBtn = document.getElementById("uploadBtn");
 const fileInput = document.getElementById("fileInput");
 const result = document.getElementById("result");
+
+// 修改成你的 Render URL
+const RENDER_URL = "https://sky-height-checker-2.onrender.com/upload";
 
 uploadBtn.addEventListener("click", async () => {
   if (!fileInput.files.length) {
     result.innerText = "請先選擇圖片";
     return;
   }
-
   const file = fileInput.files[0];
   const form = new FormData();
   form.append("image", file);
@@ -23,14 +22,13 @@ uploadBtn.addEventListener("click", async () => {
       body: form
     });
     const data = await resp.json();
-
     if (data.ok) {
-      result.innerHTML = `上傳成功<br>身高編號：${data.height}<br>Cloudinary 圖片：<a href="${data.url}" target="_blank">點我查看</a>`;
+      result.innerHTML = `上傳成功：${data.filename}<br>最接近身高編號：${data.height_id}<br><a href="${data.cloud_url}" target="_blank">Cloud 圖片</a>`;
     } else {
       result.innerText = "錯誤：" + (data.error || "未知錯誤");
     }
   } catch (e) {
-    result.innerText = "無法連線到後端，請確認網站是否已部署。";
+    result.innerText = "無法連線到後端，請確認 Flask 伺服器是否正在執行。";
     console.error(e);
   }
 });
